f62c1ee1a4697063692e920a8a423148764686be
justnada <naddyyum@gmail.com>
2021-12-31 10:54:53 +0700

    add student configuration

---
+++ src/main/java/com/nadasproject/restspringmongo/StudentConfiguration.java
@@ -0,0 +1,72 @@
+package com.nadasproject.restspringmongo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Configuration
+public class StudentConfiguration {
+
+    @Bean
+    CommandLineRunner runner(
+            StudentRepository studentRepository,
+            MongoTemplate mongoTemplate) {
+        return args -> {
+
+            Address address = new Address("England","London","NE43");
+
+            String email = "robertpatt@gmail.com";
+
+            Student student = new Student(
+                    "Robert",
+                    "Pattinson",
+                    email,
+                    Gender.MALE,
+                    address,
+                    List.of("Art", "Theater"),
+                    BigDecimal.TEN,
+                    LocalDateTime.now()
+            );
+
+            studentRepository.findStudentByEmail(email)
+                    .ifPresentOrElse(student1 -> {
+                        System.out.println(student + "\n Already exists!");
+                    }, () -> {
+                        System.out.println("Inserting student...\n" + student);
+                        studentRepository.insert(student);
+                        System.out.println("\nSuccessfully inserted!");
+                    });
+
+            // usingMongoTemplateAndQuery(studentRepository, mongoTemplate, email, student);
+
+        };
+    }
+
+    private void usingMongoTemplateAndQuery(StudentRepository studentRepository, MongoTemplate mongoTemplate, String email, Student student) {
+        Query query = new Query();
+        query.addCriteria(Criteria.where("email").is(email));
+
+        List<Student> students = mongoTemplate.find(query, Student.class);
+
+        if (students.size() > 1) {
+            throw new IllegalStateException(
+                    "found many students with email " + email);
+        }
+
+        if (students.isEmpty()) {
+            System.out.println("Inserting student...\n" + student);
+            studentRepository.insert(student);
+            System.out.println("\nSuccessfully inserted!");
+        } else {
+            System.out.println(students + "\n Already exists!");
+        }
+    }
+
+}
--- src/main/java/com/nadasproject/restspringmongo/StudentRepository.java
+++ src/main/java/com/nadasproject/restspringmongo/StudentRepository.java
@@ -1,9 +1,11 @@
 package com.nadasproject.restspringmongo;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface StudentRepository extends MongoRepository<Student, String> {
     Optional<Student> findStudentByEmail(String email);
 }
